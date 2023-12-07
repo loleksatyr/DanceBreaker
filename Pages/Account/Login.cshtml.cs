7698e8a52c79b897bc536ac9aea597895bb694e2
loleksatyr <czarnotabartek@gmail.com>
2023-12-07 18:37:01 +0100

    Dodaj pliki projektów.

---
+++ Pages/Account/Login.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace DanceBreaker.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+
+        [BindProperty]
+        public Credentail Credentails { get; set; } 
+        public void OnGet()
+        {
+            
+        }
+
+        public void OnPost() { 
+        
+        
+        }
+        public class Credentail
+        {
+            [Required]
+            [Display(Name = "Podaj numer telefonu")]
+            public int Number { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name ="Podaj PIN")]
+            public string Password { get; set; }
+        }
+    }
+}
